3ff02656f68b4633dd6452c4a4c92275b0d00ca2
Johannes Ehm <johannes.ehm@gmail.com>
2022-01-18 22:38:17 +0100

    Implements toggle picker

--- frontend/src/Components/LoginPage/LoginPage.tsx
+++ frontend/src/Components/LoginPage/LoginPage.tsx
@@ -1,5 +1,5 @@
 import { RefObject } from 'preact';
-import { SketchPicker } from 'react-color';
+import { GithubPicker } from 'react-color';
 import { useEffect, useRef, useState } from 'preact/hooks';
 import { WebSocketApi } from '../../types/WebSocket';
 import { connectToWebSocket } from '../WebSocket/WebSocket';
@@ -18,7 +18,13 @@ const isSSR = typeof window === 'undefined';
 
 const ProtoLoginPage = ({socket}: {socket: WebSocketApi}) => {
   const [user, setUser] = useState(socket.loginData.user);
+  const [color, setColor] = useState("")
+  const [colorPicker, setColorPicker] = useState(false)
   let sessionId = 'not implemented';
+
+  function handleChange(color, event) {
+    setColor(color.hex);
+  }
 
   return (
     <form class={classes.loginPage} onSubmit={(event) => {
@@ -28,10 +34,17 @@ const ProtoLoginPage = ({socket}: {socket: WebSocketApi}) => {
       <label for="user" class={classes.userLabel}>{LABEL_USERNAME}</label>
       <input id="user" type="text" value={user} class={classes.userInput} onInput={(event) => setUser((event.target as HTMLInputElement).value)} />
       <label for="color" class={classes.colorLabel}>{LABEL_COLOR}</label>
-      <span>
-        <input id="color" type="text" value="0x23434" class={classes.colorLink} onInput={(event) => setUser((event.target as HTMLInputElement).value)} />
+      <input id="color" type="text" value={color} class={classes.colorLink} onInput={(event) => setColor((event.target as HTMLInputElement).value)} />
+      <span class={classes.colorArrow} onClick={(event) => setColorPicker(!colorPicker)}>
+        {colorPicker
+          ? <span id="colorArrow" class={classes.arrowUp} onClick={(event) => setColorPicker(!colorPicker)} />
+          : <span id="colorArrow" class={classes.arrowDown} onClick={(event) => setColorPicker(!colorPicker)} />
+        }
       </span>
-      <span id="picker" class={classes.colorPicker}><SketchPicker /></span>
+        {colorPicker
+          ? <span id="picker" class={classes.colorPicker}><GithubPicker onChange={handleChange} /></span>
+          : <span />
+        }
       <label for="session" class={classes.sessionLabel}>{LABEL_SESSION}</label>
       <input id="session" type="text" value="1234" class={classes.sessionLink} />
       <input type="submit" value={BUTTON_LOGIN} class={classes.submit} />
