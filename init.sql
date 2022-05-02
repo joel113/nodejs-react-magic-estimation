7913baa4e5a3c65d9709fbd70c678ccbd8d0b4b9
Johannes Ehm <johannes.ehm@gmail.com>
2022-05-02 20:26:21 +0200

    Adds created_at and updated_at fields to init.sql

--- init.sql
+++ init.sql
@@ -12,15 +12,26 @@ CREATE TABLE elements (
     votes integer not null,
     votes_round integer not null,
     element_state element_state not null,
+    created_at timestamp not null DEFAULT Now(),
+    updated_at timestamp not null DEFAULT Now(),
     PRIMARY KEY(session_id, element_id));
 
 CREATE TABLE rounds (
     session_id char(8) PRIMARY KEY,
     rounds integer not null,
-    rounds_active integer not null);
+    rounds_active integer not null,
+    created_at timestamp not null DEFAULT Now(),
+    updated_at timestamp not null DEFAULT Now());
 
 CREATE TABLE users (
     session_id char(8) not null,
     user_id varchar(255) not null,
     color varchar(255) not null,
+    created_at timestamp not null DEFAULT Now(),
     PRIMARY KEY(session_id, user_id));
+
+DELETE FROM elements WHERE updated_at < now() - interval '7 days'
+
+DELETE FROM rounds WHERE updated_at < now() - interval '7 days'
+
+DELETE FROM users WHERE created_at < now() - interval '7 days'
