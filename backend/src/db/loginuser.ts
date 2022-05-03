54ee3af952aa324355df6e467fb2f1918a827ed0
Johannes Ehm <johannes.ehm@gmail.com>
2022-05-03 22:38:18 +0200

    Backend query code refactoring and login diaglo session id state improvement

--- backend/src/db/loginuser.ts
+++ backend/src/db/loginuser.ts
@@ -3,12 +3,16 @@ import { Client } from 'pg';
 export async function loginUser(userId: String, color: String, sessionId: String, client: Client) {
     console.log("[Magic] Received login message: %s, %s, %s", userId, color, sessionId)
     const query = 'INSERT into users(session_id, user_id, color) VALUES($1, $2, $3)'
+    executeInsert(client, query, sessionId, userId, color);
+}
+
+function executeInsert(client: Client, query: string, sessionId: String, userId: String, color: String) {
     client.query(query, [sessionId, userId, color], (err, res) => {
-        if(err) {
-            console.error("[Magic] Error when trying to insert: %s", err)
+        if (err) {
+            console.error("[Magic] Error when trying to insert: %s", err);
         }
         else {
-            console.log("[Magic] Inserted %d rows", res.rowCount)
+            console.log("[Magic] Inserted %d rows", res.rowCount);
         }
-    })
+    });
 }
--- backend/src/db/rounds.ts
+++ backend/src/db/rounds.ts
@@ -2,60 +2,47 @@ import { Client } from 'pg';
 
 export async function clearVotes(session_id: String, client: Client) {
     const query = 'UPDATE elements set votes = 0, votes_round = 0 WHERE session_id = $1'
-    client.query(query, [session_id], (err, res) => {
-        if(err) {
-            console.log("Error when trying to update: %s", err)
-        }
-        else {
-            console.log("Updatd %d rows", res.rowCount)
-        }
-    })
+    executeUpdate(client, query, session_id);
 }
 
 export async function initRounds(session_id: String, client: Client) {
     console.log("[Magic] Received init rounds message: %s", session_id)
     const query = 'INSERT into rounds(session_id, rounds, round_active) VALUES($1, 0, 0)'
-    client.query(query, [session_id], (err, res) => {
-        if(err) {
-            console.log("[Magic] Error when trying to insert: %s", err)
-        }
-        else {
-            console.log("[Magic] Inserted %d rows", res.rowCount)
-        }
-    })
+    executeInsert(client, query, session_id);
 }
 
 export async function addRound(session_id: String, client: Client) {
     console.log("[Magic] Received add round message: %s", session_id)
     const query = 'UPDATE rounds set rounds = rounds + 1, updated_at=now() WHERE session_id = $1'
-    client.query(query, [session_id], (err, res) => {
-        if(err) {
-            console.log("[Magic] Error when trying to update: %s", err)
-        }
-        else {
-            console.log("[Magic] Updatd %d rows", res.rowCount)
-        }
-    })
+    executeUpdate(client, query, session_id);
 }
 
 export async function nextRound(session_id: String, client: Client) {
     console.log("[Magic] Received next round message: %s", session_id)
     const query_rounds = 'UPDATE rounds set round_active = round_active + 1, updated_at=now() WHERE session_id = $1'
-    client.query(query_rounds, [session_id], (err, res) => {
-        if(err) {
-            console.log("[Magic] Error when trying to update: %s", err)
-        }
-        else {
-            console.log("[Magic] Updatd %d rows", res.rowCount)
-        }
-    })
     const query_elements = 'UPDATE elements set votes_round = 0, updated_at=now() WHERE session_id = $1'
-    client.query(query_elements, [session_id], (err, res) => {
-        if(err) {
-            console.log("[Magic] Error when trying to update: %s", err)
+    executeUpdate(client, query_rounds, session_id);
+    executeUpdate(client, query_elements, session_id);
+}
+
+function executeUpdate(client: Client, query: string, session_id: String) {
+    client.query(query, [session_id], (err, res) => {
+        if (err) {
+            console.log("Error when trying to update: %s", err);
         }
         else {
-            console.log("[Magic] Updatd %d rows", res.rowCount)
+            console.log("Updatd %d rows", res.rowCount);
         }
-    })
+    });
+}
+
+function executeInsert(client: Client, query: string, session_id: String) {
+    client.query(query, [session_id], (err, res) => {
+        if (err) {
+            console.log("[Magic] Error when trying to insert: %s", err);
+        }
+        else {
+            console.log("[Magic] Inserted %d rows", res.rowCount);
+        }
+    });
 }
